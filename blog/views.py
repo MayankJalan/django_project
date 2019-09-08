4542330d1e814a4ebaaf31b1c712a660f2b6d1d7
Mayank <mnkjln780@gmail.com>
2019-09-09 01:39:23 +0530

    Added comment section for every post

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,36 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.models import User
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def post_list(request):
 	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request,'blog/post_list.html',{'posts' :posts})
-
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
+@login_required
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 	return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
-    
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -44,6 +46,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
@@ -57,3 +60,16 @@ def post_edit(request, pk):
 	else:
 	    form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
