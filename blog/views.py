90d8dc6979d4068a3b014d9a82397b29621eda60
Mayank <mnkjln780@gmail.com>
2020-12-10 22:45:57 +0530

    Dhango final project

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import login_required
 def post_list(request):
 	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request,'blog/post_list.html',{'posts' :posts})
+
+
 @login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -19,11 +21,13 @@ def post_detail(request, pk):
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -53,7 +57,7 @@ def post_edit(request, pk):
 	    form = PostForm(request.POST, instance=post)
 	    if form.is_valid():
 	        post = form.save(commit=False)
-	        post.author = request.user
+	        post.aurthor = request.user
 	        
 	        post.save()
 	        return redirect('post_detail', pk=post.pk)
